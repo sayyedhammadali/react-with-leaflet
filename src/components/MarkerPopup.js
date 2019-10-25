c2719c5617e4eadb99c82d161a57a13fb46ad0a3
sayyed hammad ali <sayyed.h.ali@lucid.berlin>
2019-10-25 17:39:50 +0200

    *

--- src/components/MarkerPopup.js
+++ src/components/MarkerPopup.js
@@ -3,7 +3,6 @@ import {Popup} from 'react-leaflet';
 
 const MarkerPopup = (props) => {
   const { name } = props.data;
-  console.log(name);
 
   return  (<Popup>
     <div className='poup-text'>{name}</div>
